cb7c210659c7478b25c72a004c6c46304b52c6b1
Brandon Pineda <Brandonpineda4@gmail.com>
2019-08-29 11:46:02 -0600

    Conexion y Prueba Visual

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -3,15 +3,22 @@
 * Prueba No. 1 para control acceso biometrico UMG
 */
 
-
 package Controlador;
 
+import Modelo.Conexion;
+import Vistas.PruebaVisual;
 
 public class Main {
     
-    public static void main(String[] arg)
+    public static void main(String[] arg) throws ClassNotFoundException
     {
         
-    }
-    
+        
+      Conexion con  = new Conexion();
+      con.conexion_db();
+      con.close();
+        
+      PruebaVisual pv=new PruebaVisual();
+      pv.setVisible(true);
+    }  
 }
